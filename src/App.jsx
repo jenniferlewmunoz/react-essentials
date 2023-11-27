2e0e5989323863b3a02c1c33ddfa0ea85243396a
Jennifer J Lew Munoz <jenniferlewmunoz@gmail.com>
2023-11-27 15:32:43 -0800

    Changing the code format

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,18 @@
-// This is called a React Hook (anything that starts with use)
-import { useState } from 'react';
-import { CORE_CONCEPTS } from './data.js';
-import { EXAMPLES } from './data.js';
-
-import componentsImage from './assets/components.png';
-import propsImage from './assets/config.png';
-import jsxImage from './assets/jsx-ui.png';
-import stateImage from './assets/state-mgmt.png';
-
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from './components/TabButton';
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
 
 
 function App() {
 
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);
-  }
-
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
-          </ul>
-        </section>
-        <section id="examples">
-          <menu>
-            <TabButton isSelected={selectedTopic === "components"} onSelect={() => handleSelect("components")}>Components</TabButton>
-            <TabButton isSelected={selectedTopic === "jsx"} onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === "props"} onSelect={() => handleSelect("props")}> Props</TabButton>
-            <TabButton isSelected={selectedTopic === "state"} onSelect={() => handleSelect("state")}>State</TabButton>
-          </menu>
-          {!selectedTopic ? <p>Please select a topic.</p> : (<div id="tab-content"><h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre></div>
-          )}
-
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
